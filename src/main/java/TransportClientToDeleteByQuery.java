c030f31e398514ca39ddaae2ee95a5a3fa913dda
Vitalii <loto9308@gmail.com>
2019-01-29 11:43:32 +0200

    Delete by query added

---
+++ src/main/java/TransportClientToDeleteByQuery.java
@@ -0,0 +1,43 @@
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.BulkByScrollResponse;
+import org.elasticsearch.index.reindex.DeleteByQueryAction;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+// The delete by query API allows one to delete a given set of documents based on the result of a query:
+public class TransportClientToDeleteByQuery {
+    public static void main(String[] args) throws UnknownHostException {
+        TransportClient client = new PreBuiltTransportClient(Settings.EMPTY);
+        client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
+
+        BulkByScrollResponse response = DeleteByQueryAction.INSTANCE.newRequestBuilder(client)
+                .filter(QueryBuilders.matchQuery("user", "vitalii")) // query
+                .source("twitter") // index
+                .get(); //execute operation
+        long deleted = response.getDeleted(); // number of deleted documents
+        System.out.println(response.toString());
+
+        client.close();
+
+        /* call execute() instead of get(), to do it asynchronously and provide listener
+        DeleteByQueryAction.INSTANCE.newRequestBuilder(client)
+                .filter(QueryBuilders.matchQuery("gender", "male"))
+                .source("persons")
+                .execute(new ActionListener<BulkByScrollResponse>() {
+                    @Override
+                    public void onResponse(BulkByScrollResponse response) {
+                        long deleted = response.getDeleted();
+                    }
+                    @Override
+                    public void onFailure(Exception e) {
+                        // Handle the exception
+                    }
+                });*/
+    }
+}
